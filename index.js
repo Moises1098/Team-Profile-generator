474a3e4d34ec1339ab67fb9d99c9d5ee53e89808
Moises <moises109811@gmail.com>
2022-03-16 01:07:41 -0700

    added README and finished html genterator for intern and engenieer and formated it.

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const Intern = require('./lib/Intern');
 const teamMembers = []
 
 const promptManager = () => {
-   inquirer.prompt([
+  inquirer.prompt([
     {
       type: 'input',
       name: 'name',
@@ -30,15 +30,15 @@ const promptManager = () => {
       name: 'officeNumber',
       message: 'What is the office number of the team manager?',
     },
-   
+
 
 
   ])
-  .then((manager)=> {
-    const newManager = new Manager(manager.name, manager.id, manager.email, manager.officeNumber)
-    teamMembers.push(newManager)
-    promptMenu()
-  }) 
+    .then((manager) => {
+      const newManager = new Manager(manager.name, manager.id, manager.email, manager.officeNumber)
+      teamMembers.push(newManager)
+      promptMenu()
+    })
 };
 
 const promptEngineer = () => {
@@ -65,41 +65,41 @@ const promptEngineer = () => {
     },
 
   ])
-  .then((engineer)=> {
-    const newEngenieer = new Engineer(engineer.name, engineer.id, engineer.email, engineer.github)
-    teamMembers.push(newEngenieer)
-    promptMenu()
-  }) 
+    .then((engineer) => {
+      const newEngenieer = new Engineer(engineer.name, engineer.id, engineer.email, engineer.github)
+      teamMembers.push(newEngenieer)
+      promptMenu()
+    })
 }
 
 const promptIntern = () => {
   return inquirer.prompt([
-  {
-    type: 'input',
-    name: 'name',
-    message: 'What is the name of the intern?',
-  },
-  {
-    type: 'input',
-    name: 'id',
-    message: 'What is the ID of the intern?',
-  },
-  {
-    type: 'input',
-    name: 'email',
-    message: 'What is the email address of the intern?',
-  },
-  {
-    type: 'input',
-    name: 'school',
-    message: 'What is the school of the intern?',
-  },
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is the name of the intern?',
+    },
+    {
+      type: 'input',
+      name: 'id',
+      message: 'What is the ID of the intern?',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is the email address of the intern?',
+    },
+    {
+      type: 'input',
+      name: 'school',
+      message: 'What is the school of the intern?',
+    },
   ])
-  .then((intern)=> {
-    const newIntern = new Intern(intern.name, intern.id, intern.email, intern.school)
-    teamMembers.push(newIntern)
-    promptMenu()
-  }) 
+    .then((intern) => {
+      const newIntern = new Intern(intern.name, intern.id, intern.email, intern.school)
+      teamMembers.push(newIntern)
+      promptMenu()
+    })
 }
 
 const promptMenu = () => {
@@ -109,84 +109,126 @@ const promptMenu = () => {
       name: 'menu',
       message: 'what would you like to create next?',
       choices: ['intern', 'engineer', 'exit']
-      
+
     }
   ])
-  .then((menu)=> {
-    if (menu.menu === 'intern') {
-      promptIntern()
-    }else if(menu.menu === 'engineer') {
-      promptEngineer()
-    }else {
-      //continue with creating html
-      writer()
-    }
-  })
+    .then((menu) => {
+      if (menu.menu === 'intern') {
+        promptIntern()
+      } else if (menu.menu === 'engineer') {
+        promptEngineer()
+      } else {
+        //continue with creating html
+        writer()
+      }
+    })
 }
 
 
 
-const generateHTML = (data) =>{
+const generateHTML = (data) => {
   const teamHTMLGen = () => {
-  const teamHTML = []
-  for (var i = 0; i < data.length; i++){
-    if (data[i].getRole() === 'Manager'){
-     teamHTML.push(`<div class="card">
-        <div class="content">
-          <div class="header">${data[i].name}</div>
-          <div class="meta">Manager</div>
+    const teamHTML = []
+    for (var i = 0; i < data.length; i++) {
+      if (data[i].getRole() === 'Manager') {
+        teamHTML.push(`<div class="card">
+        <div style="background: linear-gradient(180deg, rgba(255,255,255,1) 0%, rgba(174,196,255,1) 66%, rgba(124,160,255,1) 100%);" class="content">
+          <div class="header"><i class="user icon"></i> ${data[i].name}</div>
+          <div class="meta"><i style="color: black" class="coffee icon"></i> Manager</div>
+          <div class="ui fitted divider"></div>
           <div class="ui list">
-          <div class="item">
+
+            <div class="item">
               <i class="id badge icon"></i>
-              <div class="content">
-              ID: ${data[i].id}
-              </div>
-          </div>
-      
-          <div class="item">
+                <div class="content">
+                ID: ${data[i].id}
+                </div>
+            </div>
+        
+            <div class="item">
               <i class="mail icon"></i>
-             <div class="content"> Email:
-                  <br>
-              <a href="mailto:${data[i].email}">${data[i].name}</a>
+              <div class="content"> Email: <a href="mailto:${data[i].email}">${data[i].email}</a></div>
+            </div>
+
+            <div class="item">
+              <i class="building outline icon"></i>
+              <div class="content"> 
+              Office Number: ${data[i].officeNumber}
               </div>
-          </div>
-          <div class="item">
-         <div class="content"> 
-         OfficeNumber: ${data[i].officeNumber}
-     
-          </div>
-      </div>
-          </div>
-        </div>
-      </div>`)
-    }
-    if (data[i].getRole() === 'Intern'){
-      teamHTML.push(`<div class="card">
-      <div class="content">
-        <div class="header">Veronika Ossi</div>
-        <div class="meta">Friend</div>
-        <div class="ui list">
-        <div class="item">
-            <i class="id badge icon"></i>
-            <div class="content">
-            ID: 
             </div>
+
+          </div>
         </div>
-    
-        <div class="item">
-            <i class="mail icon"></i>
-           <div class="content"> Email:
-                <br>
-            <a href="mailto:"></a>
+        </div>`)
+      }
+      if (data[i].getRole() === 'Engineer') {
+        teamHTML.push(`
+
+        <div class="card">
+        <div style="background: linear-gradient(180deg, rgba(255,255,255,1) 0%, rgba(174,196,255,1) 66%, rgba(124,160,255,1) 100%);" class="content">
+          <div class="header"><i class="user icon"></i> ${data[i].name}</div>
+          <div class="meta"><i style="color: black" class="pencil alternate icon"></i> Engineer</div>
+          <div class="ui fitted divider"></div>
+          <div class="ui list">
+
+            <div class="item">
+              <i class="id badge icon"></i>
+                <div class="content">
+                ID: ${data[i].id}
+                </div>
             </div>
+        
+            <div class="item">
+              <i class="mail icon"></i>
+              <div class="content"> Email: <a href="mailto:${data[i].email}">${data[i].email}</a></div>
+            </div>
+
+            <div class="item">
+              <i class="building outline icon"></i>
+              <div class="content"> 
+              Office Number: ${data[i].officeNumber}
+              </div>
+            </div>
+
+            <div class="item">
+                <i class="github icon"></i>
+                <div class="content">
+                GitHub: ${data[i].github}
+                </div>
+            </div>
+          </div>
         </div>
+        </div>`)
+      }
+      if (data[i].getRole() === 'Intern') {
+        teamHTML.push(`
+
+        <div class="card">
+        <div style="background: linear-gradient(180deg, rgba(255,255,255,1) 0%, rgba(174,196,255,1) 66%, rgba(124,160,255,1) 100%);" class="content">
+          <div class="header"><i class="user icon"></i> ${data[i].name}</div>
+          <div class="meta"><i style="color: black" class="graduation cap icon"></i> Intern</div>
+          <div class="ui fitted divider"></div>
+          <div class="ui list">
+
+            <div class="item">
+              <i class="id badge icon"></i>
+                <div class="content">
+                ID: ${data[i].id}
+                </div>
+            </div>
+        
+            <div class="item">
+              <i class="mail icon"></i>
+              <div class="content"> Email: <a href="mailto:${data[i].email}">${data[i].email}</a></div>
+            </div>
+            
+          </div>
         </div>
-      </div>
-    </div>`) 
+        </div>`)
+      }
     }
+    return teamHTML.join('')
   }
-  return teamHTML.join('')
-}
 
   return `<!DOCTYPE html>
 <html lang="en">
@@ -198,60 +240,14 @@ const generateHTML = (data) =>{
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css" integrity="sha512-8bHTC73gkZ7rZ7vpqUQThUDhqcNFyYi2xgDgPDHc+GXVGHXq+xPjynxIopALmOPqzo9JZj0k6OqqewdGO3EsrQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>My Team</title>
 </head>
+
 <body>
-    <header><h1 style="background:red;">My Team</h1></header>
-
-    <section>
-        <div class="ui cards">
-           ${teamHTMLGen()}
-            <div class="card">
-              <div class="content">
-                <div class="header">Veronika Ossi</div>
-                <div class="meta">Friend</div>
-                <div class="ui list">
-                <div class="item">
-                    <i class="id badge icon"></i>
-                    <div class="content">
-                    ID: 
-                    </div>
-                </div>
-            
-                <div class="item">
-                    <i class="mail icon"></i>
-                   <div class="content"> Email:
-                        <br>
-                    <a href="mailto:"></a>
-                    </div>
-                </div>
-                </div>
-              </div>
-            </div>
-            <div class="card">
-              <div class="content">
-                <div class="header">Jenny Hess</div>
-                <div class="meta">Friend</div>
-                <div class="ui list">
-                <div class="item">
-                    <i class="id badge icon"></i>
-                    <div class="content">
-                    ID: 
-                    </div>
-                </div>
-            
-                <div class="item">
-                    <i class="mail icon"></i>
-                    <div class="content"> Email:
-                        <br>
-                    <a href=""></a>
-                    </div>
-                </div>
-                </div>
-              </div>
-            </div>
-          </div>
-    </section>
-    
-
+<header> <div class="ui block aligned center header" style="background-color:red; font-size: xx-large; color: white;" >My Team</div> </header>
+  <section style="padding: 1rem;">
+  <div class="ui stackable cards">
+    ${teamHTMLGen()}
+  </div>
+  </section>
 
 </body>
 </html>`};
@@ -259,11 +255,11 @@ const generateHTML = (data) =>{
 
 const writer = () => {
   fs.writeFileSync('dist/index.html', generateHTML(teamMembers))
-  
-    // Use writeFileSync method to use promises instead of a callback function
-    // .then((answers) => fs.writeFileSync('index.html', generateHTML(answers)))
-    // .then(() => console.log('Successfully wrote to index.html'))
-    // .catch((err) => console.error(err));
+
+  // Use writeFileSync method to use promises instead of a callback function
+  // .then((answers) => fs.writeFileSync('index.html', generateHTML(answers)))
+  // .then(() => console.log('Successfully wrote to index.html'))
+  // .catch((err) => console.error(err));
 };
 
 
